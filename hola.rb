bc8a1cfd1d0f3db4afe9e34aeea46fe89741c82b
Luis Pedro Sarti <sartip@zauru.com>
2014-08-11 16:40:38 -0600

    cambios

--- hola.rb
+++ hola.rb
@@ -1,6 +1,6 @@
 bienvenida = "bienvenidoz al ejepmlo (algo mas de info) de como utilizar el ocoto gato\n"+
-	"para este caso usaremos merch"
+	"para este caso usaremos merge"
 
 puts bienvenida
 
-puts "hola mundo, verde!"
+puts "hola mundo!"
